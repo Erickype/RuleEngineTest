c73e5b305de36eee7faa8d3bce762f9aa05a75d1
Erickype <erickype@hotmail.com>
2023-08-01 17:43:58 -0500

    Structs to hold the expert systems and project that use expert systems

--- main.go
+++ main.go
@@ -13,5 +13,19 @@ const (
 	NonLinear   clips.Symbol = "NONLINEAR"
 )
 
+// ExpertSystem is a candidate expert system
+type ExpertSystem struct {
+	License  clips.Symbol   `json:"license"`
+	Language []clips.Symbol `json:"language"`
+}
+
+// Project represents a project that might need an expert system
+type Project struct {
+	Logic                   clips.Symbol
+	Language                []clips.Symbol
+	IsExpertSystemCandidate bool
+	PreferredShell          *ExpertSystem
+}
+
 func main() {
 }
