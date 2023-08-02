e82dc12bc6dcb9560c136d379586fcfd8cd9ccd4
Erickype <erickype@hotmail.com>
2023-08-02 10:49:13 -0500

    Use rules to build knowledge base

--- main.go
+++ main.go
@@ -1,125 +1,21 @@
 package main
 
-import "github.com/keysight/clipsgo/pkg/clips"
-
-const (
-	Free       clips.Symbol = "FREE"
-	Commercial clips.Symbol = "COMMERCIAL"
-	Dubious    clips.Symbol = "DUBIOUS"
+import (
+	_ "embed"
+	"github.com/hyperjumptech/grule-rule-engine/ast"
+	"github.com/hyperjumptech/grule-rule-engine/builder"
+	"github.com/hyperjumptech/grule-rule-engine/pkg"
 )
 
-const (
-	Algorithmic clips.Symbol = "ALGORITHMIC"
-	NonLinear   clips.Symbol = "NONLINEAR"
-)
-
-var expertSystems = []*ExpertSystem{
-	{
-		Name:     "Clips",
-		License:  Free,
-		Language: []clips.Symbol{"Python", "C"},
-	},
-	{
-		Name:     "Prolog",
-		License:  Free,
-		Language: []clips.Symbol{"Prolog"},
-	}, {
-		Name:     "Jess",
-		License:  Free,
-		Language: []clips.Symbol{"Java"},
-	},
-	{
-		Name:     "Drools",
-		License:  Free,
-		Language: []clips.Symbol{"Java"},
-	},
-	{
-		Name:     "Shine",
-		License:  Dubious,
-		Language: []clips.Symbol{"C"},
-	},
-}
-
-var rules = []string{
-	`(defrule expert-system-candidate
-		(object (is-a Project) (name ?projname)
-			(Logic NONLINEAR)
-		)
-	=>
-		(send ?projname put-IsExpertSystemCandidate TRUE)
-	)`,
-	`(defrule choose-free
-		(declare (salience 200))
-		(object (is-a Project) (name ?projname)
-			(IsExpertSystemCandidate TRUE)
-			(PreferredShell [nil])
-			(Languages $? ?lang $?)
-		)
-	=> 
-		(send ?projname put-PreferredShell ?shell)
-	)`,
-	`(defrule choose-commercial
-		(declare (salience 100))
-		(object (is-a Project) (name ?projname)
-			(IsExpertSystemCandidate TRUE)
-			(PreferredShell [nil])
-			(Languages $? ?lang $?)
-		)
-		(object (is-a ExpertSystem) (name ?shell)
-			(License COMMERCIAL)
-			(Languages $? ?lang $?)
-		)
-	=>
-		(send ?projname put-PreferredShell ?shell)
-	)`,
-}
-
-// ExpertSystem is a candidate expert system
-type ExpertSystem struct {
-	Name     string
-	License  clips.Symbol   `json:"license"`
-	Language []clips.Symbol `json:"language"`
-}
-
-// Project represents a project that might need an expert system
-type Project struct {
-	Logic                   clips.Symbol
-	Language                []clips.Symbol
-	IsExpertSystemCandidate bool
-	PreferredShell          *ExpertSystem
-}
-
-func insertExpertSystems(env *clips.Environment) error {
-	for _, system := range expertSystems {
-		_, err := env.Insert(system.Name, &system)
-		if err != nil {
-			return err
-		}
-	}
-	return nil
-}
-
-func buildEnvironment(env *clips.Environment) error {
-	err := env.Build(`(
-	defmessage-handler ExpertSystem add-go-support ())
-		(send ?self put-languages (create$ self:Languages Go)
-	)`)
-	if err != nil {
-		return err
-	}
-	return nil
-}
-
-func insertRules(env *clips.Environment) error {
-	for i := 0; i < len(rules); i++ {
-		rule := rules[i]
-		err := env.Build(rule)
-		if err != nil {
-			return err
-		}
-	}
-	return nil
-}
+//go:embed check_values.grl
+var rules []byte
 
 func main() {
+	knowledgeLibrary := ast.NewKnowledgeLibrary()
+	ruleBuilder := builder.NewRuleBuilder(knowledgeLibrary)
+
+	err := ruleBuilder.BuildRuleFromResource("TutorialRules", "0.0.1", pkg.NewBytesResource(rules))
+	if err != nil {
+		panic(err)
+	}
 }
